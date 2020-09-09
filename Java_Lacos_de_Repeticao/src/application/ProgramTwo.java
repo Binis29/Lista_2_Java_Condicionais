6589535195152cc4399fd871121f5d0bda6bfc1f
binis29 <cleitonaguiarandrade@hotmail.com>
2020-09-09 14:08:30 -0300

    atualização 1

--- Java_Lacos_de_Repeticao/src/application/ProgramTwo.java
+++ Java_Lacos_de_Repeticao/src/application/ProgramTwo.java
@@ -9,16 +9,17 @@ public class ProgramTwo {
 
 	public static void main(String[] args) {
 		
+		final int VALORES = 10;
 		Scanner sc = new Scanner(System.in);
-		double [] numero = new double[3];
+		double [] numero = new double[VALORES];
 		double save;
 		
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < VALORES; i++) {
 			System.out.println("Entre com um número: ");
 			numero [i] = sc.nextDouble();
 		}
 		
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < VALORES; i++) {
 				
 			
 			for(int j=0; j<numero.length; j++)
@@ -30,8 +31,10 @@ public class ProgramTwo {
 				}
 						
 			} 
+		System.out.println();
 		
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < VALORES; i++) {
+			
 			System.out.println(numero[i]);
 		}
 		
